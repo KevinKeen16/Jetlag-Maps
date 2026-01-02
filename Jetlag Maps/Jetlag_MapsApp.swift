32f6fa85a4c44f82c7d0c76c45d66e277a033af0
Kevin Keen <kevin-keen@outlook.com>
2026-01-02 09:22:47 +0100

    Initial Commit
    All X-Code default project files

---
+++ Jetlag Maps/Jetlag_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jetlag_MapsApp.swift
+//  Jetlag Maps
+//
+//  Created by Kevin Keen on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Jetlag_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
